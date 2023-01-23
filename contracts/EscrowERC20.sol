f0b1e64fdf976b679097403bc2848713d71939ec
James Alavosus <alavosus.james@gmail.com>
2023-01-22 21:55:56 -0500

    Add contracts and gitignore:

---
+++ contracts/EscrowERC20.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {Escrow} from "@openzeppelin/contracts/utils/escrow/Escrow.sol";
+
+contract EscrowERC20 is Escrow {
+    using Address for address payable;
+
+    event ERC20Deposited(
+        address indexed payee,
+        IERC20 indexed token,
+        uint256 amount
+    );
+
+    event ERC20Withdrawn(
+        address indexed payee,
+        IERC20 indexed token,
+        uint256 amount
+    );
+
+    mapping(address => mapping(IERC20 => uint256)) private _erc20Deposits;
+
+    constructor() Escrow() {}
+
+    function depositsOf(address payee, IERC20 token)
+        public
+        view
+        returns (uint256)
+    {
+        return _erc20Deposits[payee][token];
+    }
+
+    /**
+     * @dev Stores the sent token amount as credit to be withdrawn.
+     * @param payee The destination address of the funds.
+     * @param token IERC20 token being deposited
+     * @param amount amount of {token} being deposited
+     *
+     * Emits a {ERC20Deposited} event.
+     */
+    function deposit(address payee, IERC20 token, uint256 amount)
+        public
+        onlyOwner
+    {
+        require(
+            token.balanceOf(msg.sender) >= amount,
+            "sender balance too low"
+        );
+
+        require(
+            token.allowance(msg.sender, address(this)) >= amount,
+            "allowance too low"
+        );
+
+        _depositERC20(payee, token, amount);
+
+        emit ERC20Deposited(payee, token, amount);
+    }
+
+    function _depositERC20(address payee, IERC20 token, uint256 amount)
+        internal
+        onlyOwner
+    {
+        token.transferFrom(msg.sender, address(this), amount);
+        _erc20Deposits[payee][token] += amount;
+    }
+
+    /**
+     * @dev Withdraw accumulated token balance for a payee, forwarding all gas to the
+     * recipient.
+     *
+     * WARNING: Forwarding all gas opens the door to reentrancy vulnerabilities.
+     * Make sure you trust the recipient, or are either following the
+     * checks-effects-interactions pattern or using {ReentrancyGuard}.
+     *
+     * @param payee The address whose funds will be withdrawn and transferred to.
+     * @param token IERC20 token which will be withdrawn
+     *
+     * Emits a {ERC20Withdrawn} event.
+     */
+    function withdraw(address payee, IERC20 token)
+        public
+    {
+        (uint256 _amount, bool _withdrawn) = _withdrawERC20(payee, token);
+
+        require(_withdrawn, "no tokens to withdraw for address token combo");
+
+        emit ERC20Withdrawn(payee, token, _amount);
+    }
+
+    function _withdrawERC20(address payee, IERC20 token)
+        internal
+        returns (uint256, bool)
+    {
+        bool withdrawn = false;
+        uint256 amount = _erc20Deposits[payee][token];
+        if (amount > 0) {
+            token.transfer(payee, amount);
+            _erc20Deposits[payee][token] = 0;
+            withdrawn = true;
+        }
+
+        return (amount, withdrawn);
+    }
+}
---
+++ contracts/PullPaymentERC20.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import {EscrowERC20} from "./EscrowERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {PullPayment} from "@openzeppelin/contracts/security/PullPayment.sol";
+
+abstract contract PullPaymentERC20 is PullPayment {
+    EscrowERC20 private immutable _escrowERC20;
+
+    constructor() PullPayment() {
+        _escrowERC20 = new EscrowERC20();
+    }
+
+    /**
+     * @dev Withdraw accumulated ERC20 payments, forwarding all gas to the recipient.
+     *
+     * Note that _any_ account can call this function, not just the `payee`.
+     * This means that contracts unaware of the `PullPayment` protocol can still
+     * receive funds this way, by having a separate account call
+     * {withdrawPayments}.
+     *
+     * WARNING: Forwarding all gas opens the door to reentrancy vulnerabilities.
+     * Make sure you trust the recipient, or are either following the
+     * checks-effects-interactions pattern or using {ReentrancyGuard}.
+     *
+     * @param payee Whose payments will be withdrawn.
+     * @param token Token to withdraw.
+     *
+     * Causes the `escrow` to emit an {ERC20Withdrawn} event.
+     */
+    function withdrawPayments(address payee, IERC20 token)
+        public
+        virtual
+    {
+        _escrowERC20.withdraw(payee, token);
+    }
+
+    /**
+     * @dev Returns the ERC20 payments of a specific token owed to an address.
+     * @param dest The creditor's address.
+     * @param token Token owed.
+     */
+    function payments(address dest, IERC20 token)
+        public
+        virtual
+        view
+        returns (uint256)
+    {
+        return _escrowERC20.depositsOf(dest, token);
+    }
+
+    /**
+     * @dev Called by the payer to store the sent amount as credit to be pulled.
+     * Funds sent in this way are stored in an intermediate {EscrowERC20} contract, so
+     * there is no danger of them being spent before withdrawal.
+     *
+     * @param dest The destination address of the funds.
+     * @param amount The amount to transfer.
+     * @param token The token to transfer.
+     *
+     * Causes the `escrow` to emit a {ERC20Deposited} event.
+     */
+    function _asyncTransfer(address dest, IERC20 token, uint256 amount)
+        internal
+        virtual
+    {
+        _escrowERC20.deposit(dest, token, amount);
+    }
+}
